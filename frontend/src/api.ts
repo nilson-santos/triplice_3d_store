5b35c4d9cdad3c06d1285c0659c4352a73197d8e
Nilson Santos <contato@nilson.com.br>
2026-02-02 23:34:46 -0300

    adjust

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://api.triplice3d.com.br:8000/api',
+    baseURL: 'https://api.triplice3d.com.br/api',
     headers: {
         'Content-Type': 'application/json',
     },
