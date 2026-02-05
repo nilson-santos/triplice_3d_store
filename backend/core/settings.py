9bc38a19766963acf912af60a4bbee1a44516a36
Nilson Santos <contato@nilson.com.br>
2026-02-05 04:48:02 -0300

    feat: add proxy and HTTPS related settings for Django.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,6 +29,10 @@ DEBUG = os.environ.get('DEBUG', '1') == '1'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+# Proxy/HTTPS Settings
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
 
 # Application definition
 
